7f8048c86b5d6bb5f2ba7fe0e710edcd675cce01
Shrinijoshi <Shrinijoshi@users.noreply.github.com>
2021-09-16 17:36:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import EX01 from "./component/EX01";
 import "./style.css";
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Hello Joshi!</h1>
+      <EX01 />
     </div>
   );
 }
