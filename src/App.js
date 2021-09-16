8a1e938dc55af24a3943edb10fb1c966fc0c0d22
Shrinijoshi <josh.shrinivas@gmail.com>
2021-09-16 17:30:05 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import EX01 from './components/EX01';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <EX01 />
     </div>
   );
 }
---
+++ src/component/EX01.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EX01 = () => {
+  const element = <h6>Hello, Stuthi! </h6>;
+
+  return element;
+};
+
+export default EX01;
